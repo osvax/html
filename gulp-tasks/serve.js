bb8d0e8b4772f2e868c3321f53c8dd7ce1cbbb3d
Valentin Alexo <osvax@yandex.ru>
2020-04-05 02:09:54 +0400

    replace gulp-scss-starter

---
+++ gulp-tasks/serve.js
@@ -0,0 +1,46 @@
+"use strict";
+
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import browsersync from "browser-sync";
+
+gulp.task("serve", () => {
+    browsersync.init({
+        server: "./dist/",
+        port: 4000,
+        notify: true,
+		browser:'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+		//tunnel: "http://valexo.localtunnel.me", // Демонстрация страницы: http://projectname.localtunnel.me
+		logPrefix: "Valexo_Dev",
+		// online: false, // Работа в автономном режиме без подключения к интернету
+    });
+
+    gulp.watch(paths.views.watch, gulp.parallel("views"));
+    gulp.watch(paths.styles.watch, gulp.parallel("styles"));
+    gulp.watch(paths.scripts.watch, gulp.parallel("scripts"));
+    gulp.watch(paths.sprites.watch, gulp.parallel("sprites"));
+    gulp.watch(paths.images.watch, gulp.parallel("images"));
+    gulp.watch(paths.webp.watch, gulp.parallel("webp"));
+    gulp.watch(paths.fonts.watch, gulp.parallel("fonts"));
+});
+
+gulp.task("proxy", () => {
+    browsersync.init({
+        proxy: "localhost:9000",
+		host: "localhost",
+		port: 9090,
+		browser:'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+		logPrefix: "Val_Devil_Proxy",
+		files:     [
+		"./dist/"
+		],
+    });
+
+    gulp.watch(paths.views.watch, gulp.parallel("views"));
+    gulp.watch(paths.styles.watch, gulp.parallel("styles"));
+    gulp.watch(paths.scripts.watch, gulp.parallel("scripts"));
+    gulp.watch(paths.sprites.watch, gulp.parallel("sprites"));
+    gulp.watch(paths.images.watch, gulp.parallel("images"));
+    gulp.watch(paths.webp.watch, gulp.parallel("webp"));
+    gulp.watch(paths.fonts.watch, gulp.parallel("fonts"));
+});
